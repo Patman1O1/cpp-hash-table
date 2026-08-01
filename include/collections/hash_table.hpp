505d6e53df89e3916f5dd5259522dda42f18cda1
Patrick Thomas <patrickthomas101x@gmail.com>
2026-07-31 22:33:47 -0500

    Remove cpp prefix

--- include/collections/hash_table.hpp
+++ include/collections/hash_table.hpp
@@ -1,4 +1,17 @@
-#ifndef CPP_HASH_TABLE_HPP
-#define CPP_HASH_TABLE_HPP
+#ifndef COLLECTIONS_HASH_TABLE_HPP
+#define COLLECTIONS_HASH_TABLE_HPP
 
-#endif // #ifndef CPP_HASH_TABLE_HPP
+// ISO C Includes
+#include <cstddef>
+
+// ISO C++ Includes
+#include <functional>
+#include <memory>
+
+namespace collections {
+    template<typename T, typename Hash = std::hash<T>, typename Equal = std::equal_to<T>, typename Allocator = std::allocator<T>>
+    class hash_table {
+    };
+} // namespace collections
+
+#endif // #ifndef COLLECTIONS_HASH_TABLE_HPP
